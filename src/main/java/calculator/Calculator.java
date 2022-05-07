cd2e7007da831d0a132e7fe10c061819c66ebf9a
JiHunHong <hongji3354@gmail.com>
2022-05-07 17:23:51 +0900

    fix : 메서드명 철자 틀림 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator {
     private int getSum(final String token) {
         try {
             final int i = Integer.parseInt(token);
-            checkNumberNegetive(token, i);
+            checkNumberNegative(token, i);
             sum += i;
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 가능합니다. 입력한 문자는 : " + token + "입니다.", e);
@@ -30,7 +30,7 @@ public class Calculator {
         return sum;
     }
 
-    private void checkNumberNegetive(final String text, final int i) {
+    private void checkNumberNegative(final String text, final int i) {
         if (i < 0) {
             throw new IllegalArgumentException("양수만 가능합니다. 입력한 숫자는 : " + text + "입니다.");
         }
