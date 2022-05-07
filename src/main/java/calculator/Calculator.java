eb5b37cb06ea6cae7b7b243fc1e11565068b79f7
JiHunHong <hongji3354@gmail.com>
2022-05-07 17:36:42 +0900

    refactor : 변수명 및 파라미터명 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,14 +2,14 @@ package calculator;
 public class Calculator {
 
     private int sum;
-    private final String[] split;
+    private final String[] tokens;
 
-    public Calculator(final String[] split) {
-        this.split = split;
+    public Calculator(final String[] tokens) {
+        this.tokens = tokens;
     }
 
     public int sum() {
-        return getSum(split);
+        return getSum(tokens);
     }
 
     private int getSum(final String[] tokens) {
