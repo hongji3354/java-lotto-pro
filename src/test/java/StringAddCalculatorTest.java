493329e9e9a5287557fbb5e4aee940c34e6072ad
JiHunHong <hongji3354@gmail.com>
2022-05-07 18:35:58 +0900

    test : 파라미터만 다른 테스트 @ParameterizedTest로 변경

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -2,6 +2,7 @@ import calculator.StringAddCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,57 +21,15 @@ class StringAddCalculatorTest {
         assertThat(sum).isZero();
     }
 
-    @Test
-    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
-    void splitAndSum_숫자하나() {
-        // given
-        String text = "1";
-
+    @ParameterizedTest
+    @CsvSource(value = {"1]1", "1,2]3", "1:2]3", "1,2:3]6"}, delimiter = ']')
+    @DisplayName("쉼표 또는 콜론 구분자를 가질 수 있다")
+    void splitAndSum(String text, Integer expectedSum) {
         // when
         int sum = StringAddCalculator.splitAndSum(text);
 
         // then
-        assertThat(sum).isEqualTo(1);
-    }
-
-
-    @Test
-    @DisplayName("쉼표를 구분자로 가지는 문자열을 전달할 경우 쉼표를 기준으로 분리한 각 숫자의 합을 반환한다.")
-    void splitAndSum_쉼표() {
-        // given
-        String text = "1,2";
-
-        // when
-        int sum = StringAddCalculator.splitAndSum(text);
-
-        // then
-        assertThat(sum).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("콜론을 구분자로 가지는 문자열을 전달할 경우 콜론을 기준으로 분리한 각 숫자의 합을 반환한다.")
-    void splitAndSum_콜론() {
-        // given
-        String text = "1,2";
-
-        // when
-        int sum = StringAddCalculator.splitAndSum(text);
-
-        // then
-        assertThat(sum).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("쉼표 또는 콜론을 구분자로 가지는 문자열을 전달할 경우 콜론을 기준으로 분리한 각 숫자의 합을 반환한다.")
-    void splitAndSum_쉼표_또는_콜론() {
-        // given
-        String text = "1,2:3";
-
-        // when
-        int sum = StringAddCalculator.splitAndSum(text);
-
-        // then
-        assertThat(sum).isEqualTo(6);
+        assertThat(sum).isEqualTo(expectedSum);
     }
 
     @Test
